52df085162a29b7be7f19eb2fb3a7181bda79381
haywhyzero <aregbeayomide@gmail.com>
2026-06-29 08:45:40 +0100

    Fix: SafeArea in dailyChallenge, also fixed the google authservice by initializing at app start and not inside the authservice itself. Also added google credentials dependency to build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -74,6 +74,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.3.0")
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 }
 
 configurations.all {
